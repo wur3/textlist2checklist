697d7df3ede01299340efc2aac0dfc1625266ccc
Richard Wu <wu.richard.98@gmail.com>
2018-08-11 16:58:21 -0400

    Testing line parsing

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from tkinter import *
 from tkinter import ttk
 
+# Section I: txt parsing
+filename = "list.txt"
+# filename = input("Name of .txt file:\n")
+textfile = open(filename, 'r')
+for line in textfile.readlines():
+    print(line.strip())
+
+# Section II: Tkinter
 root = Tk()
 root.title("Check List")
-checkbutton = ttk.Checkbutton(root, text = "TEST")
-checkbutton.pack()
+cb = ttk.Checkbutton(root, text = "TEST")
+cb.pack()
 
 root.mainloop()
