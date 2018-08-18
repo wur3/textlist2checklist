53b55379787302802f55e71bced3325ce3feeb21
Richard Wu <wu.richard.98@gmail.com>
2018-08-17 23:08:19 -0400

    Stopped using ttk (outdated?) & visible checkbutton

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from tkinter import *
-from tkinter import ttk
+#from tkinter import ttk
+
+item_list = []
 
 # Section I: txt parsing
 filename = "list.txt"
+print("opening list.txt")
 # filename = input("Name of .txt file:\n")
 textfile = open(filename, 'r')
 for line in textfile.readlines():
-    print(line.strip())
+    item_list.append(line.strip())
 
 # Section II: Tkinter
 root = Tk()
 root.title("Check List")
-cb = ttk.Checkbutton(root, text = "TEST")
-cb.pack()
+Label(root, text = "Welcome to textlist2checklist!", justify = CENTER).pack()
+for item in item_list:
+    checkvar = BooleanVar()
+    cb = Checkbutton(root, text = item, variable = checkvar, \
+                        height = 2, width = 10)
+    cb.pack()
 
 root.mainloop()
