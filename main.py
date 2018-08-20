14304cce86c400c157d54568c5b17b16cff2f6e5
Richard Wu <wu.richard.98@gmail.com>
2018-08-19 22:23:48 -0400

    Save button and aligned checkbuttons

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from tkinter import *
-#from tkinter import ttk
 
 item_list = []
 
 # Section I: txt parsing
 filename = "list.txt"
-print("opening list.txt")
+
 # filename = input("Name of .txt file:\n")
 textfile = open(filename, 'r')
 for line in textfile.readlines():
@@ -14,11 +13,12 @@ for line in textfile.readlines():
 # Section II: Tkinter
 root = Tk()
 root.title("Check List")
-Label(root, text = "Welcome to textlist2checklist!", justify = CENTER).pack()
+Label(root, text = "Welcome to textlist2checklist!", padx = 10, \
+                        pady = 4, justify = CENTER).pack()
 for item in item_list:
     checkvar = BooleanVar()
     cb = Checkbutton(root, text = item, variable = checkvar, \
-                        height = 2, width = 10)
+                        anchor = 'w', justify = CENTER, height = 1, width = 10)
     cb.pack()
-
+Button(root, text = "Save", padx = 5, pady = 2).pack()
 root.mainloop()
