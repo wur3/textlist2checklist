19876e7a4fb7dec0b208ff073cbf14d0f79a6187
Richard Wu <wu.richard.98@gmail.com>
2018-09-30 21:03:29 -0400

    Object oriented!

--- checklist.py
+++ checklist.py
@@ -1,19 +1,53 @@
 from tkinter import *
 
-class Functions:
-    def extract(filename):
-        item_list = []
-        textfile = open(filename, 'r')
+class Checklist:
+
+    def __init__(self, root):
+        self.filename = "list.txt"
+        #self.filename = input("Name of .txt file:\n")
+
+        # list of Checkbutton objects
+        self.buttons = []
+        # list of corresponding checkbox values
+        self.cb_val = []
+
+        self.root = root
+        root.title("Check List")
+        self.label = Label(root, text = "Welcome to textlist2checklist!", padx = 10, \
+                            pady = 4, justify = CENTER)
+        self.label.pack()
+
+        # list of string items extracted from file
+        self.item_list = []
+        self.item_list = self.extract()
+        self.make_list()
+
+        self.save_button = Button(self.root, text = "Save", \
+            command = lambda: self.save(), \
+            padx = 5, pady = 2)
+        self.save_button.pack()
+
+    def extract(self):
+        list = []
+        textfile = open(self.filename, 'r')
         for line in textfile.readlines():
-            item_list.append(line.strip())
+            list.append(line.strip())
         textfile.close()
-        return item_list
+        return list
 
-    def save(widget_list, filename):
+    def make_list(self):
+        for item in self.item_list:
+            checkvar = BooleanVar()
+            cb = Checkbutton(self.root, text = item, variable = checkvar, \
+                                anchor = 'w', height = 1, width = 15)
+            cb.pack()
+            self.buttons.append(cb)
+            self.cb_val.append(checkvar)
+
+    def save(self):
         #textfile = open(filename, 'w')
-        for widget in widget_list:
-
-            print (widget[0].cget("text"))
-            print (widget[1].get())
+        for i in range(len(self.buttons)):
+            print (self.buttons[i].cget("text"))
+            print (self.cb_val[i].get())
             #textfile.write() #thats how you write to a file
         #textfile.close()
--- main.py
+++ main.py
@@ -1,25 +1,6 @@
 from tkinter import *
-from func import Functions
+from checklist import Checklist
 
-### Section I: txt parsing
-file = "list.txt"
-
-# file = input("Name of .txt file:\n")
-list = Functions.extract(file)
-
-### Section II: Tkinter
 root = Tk()
-root.title("Check List")
-Label(root, text = "Welcome to textlist2checklist!", padx = 10, \
-                        pady = 4, justify = CENTER).pack()
-buttons = []
-for item in list:
-    checkvar = BooleanVar()
-    cb = Checkbutton(root, text = item, variable = checkvar, \
-                        anchor = 'w', height = 1, width = 15)
-    cb.pack()
-    buttons.append([cb, checkvar])
-Button(root, text = "Save", \
-    command = lambda: Functions.save(buttons, "list.txt"), \
-    padx = 5, pady = 2).pack()
+c = Checklist(root)
 root.mainloop()
